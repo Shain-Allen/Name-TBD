f875620fae65c4ef28192746395d4381bcbd33ad
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2019-05-28 14:01:38 -0700

    comander we have alpha

--- The Fine Spline Sine Lines/Assets/Scripts/CollectibleController.cs
+++ The Fine Spline Sine Lines/Assets/Scripts/CollectibleController.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class CollectibleController : MonoBehaviour
 {
+    [FMODUnity.EventRef]
+    public string pickupNoise;
+
+    FMOD.Studio.EventInstance PlayPickupSound;
+
     public LevelManager levelManager;
+
+
+    void Awake()
+    {
+        PlayPickupSound = FMODUnity.RuntimeManager.CreateInstance(pickupNoise);
+    }
+
 
     // Update is called once per frame
     void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.gameObject.name != "Player") return;
 
+        PlayPickupSound.start();
+        Debug.Log("heck");
+
         levelManager = GameObject.Find("Camera").GetComponent<LevelManager>();
-        
+
         levelManager.Collectibles[levelManager.currentLevel] = true;
 
         Destroy(gameObject);
