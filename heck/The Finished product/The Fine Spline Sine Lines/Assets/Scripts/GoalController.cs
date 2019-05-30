3a897f8d0d63ac796680c0b372b171c6b4f73ded
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2019-05-30 15:06:58 -0700

    heck

---
+++ heck/The Finished product/The Fine Spline Sine Lines/Assets/Scripts/GoalController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalController : MonoBehaviour
+{
+    public LevelManager LevelManager;
+
+    public int LevelToLoad = -1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LevelManager = GameObject.Find("Camera").GetComponent<LevelManager>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.name != "Player") return;
+        if (LevelToLoad == -1)
+        {
+            LevelManager.LoadNextLevel();
+        }
+        else
+        {
+            LevelManager.LoadLevel(LevelToLoad);
+        }
+        
+        Destroy(gameObject);
+    }
+}
