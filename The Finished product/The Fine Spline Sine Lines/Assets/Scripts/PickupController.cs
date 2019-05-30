181667349eef5c2217435a693d3df1ae5a723e4b
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2019-05-30 13:30:00 -0700

    we got this

---
+++ The Finished product/The Fine Spline Sine Lines/Assets/Scripts/PickupController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupController : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string pickupNoise;
+
+    FMOD.Studio.EventInstance PlayPickupSound;
+
+    public LevelManager levelManager;
+
+    public Vector2 amount = Vector2.zero;
+
+    void Awake()
+    {
+        PlayPickupSound = FMODUnity.RuntimeManager.CreateInstance(pickupNoise);
+    }
+
+    void OnTriggerEnter2D (Collider2D collider2D)
+    {
+        if (collider2D.name != "Player") return;
+
+        PlayPickupSound.start();
+        //Debug.Log("heck");
+
+        collider2D.GetComponent<Movement>().rangeChangeDir -= amount;
+
+        Destroy(gameObject);
+	}
+}
